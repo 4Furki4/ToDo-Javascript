20be8667b72463a5fd4c018334a008b5fff0665b
Furkan <muhammedcengiz1@gmail.com>
2022-09-14 16:16:47 +0300

    Listeye eleman eklerken x işareti de listenin sağ kısmına eklendi ve html gömülü liste maddeleri kaldırıldı

--- index.js
+++ index.js
@@ -6,8 +6,27 @@ let toDo = []
 
 function newElement() {
 
-    if (inputToDo.value.trim()) {
+    if (inputToDo.value.trim() || !toDo.includes(inputToDo.value)) {
         
+        toDo.push(inputToDo.value)
+        localStorage.setItem("liste", JSON.stringify(toDo))
+        addEntryToDo()
     }
 
+}
+
+
+function addEntryToDo() {
+    let list = JSON.parse(localStorage.getItem("liste"))
+    list.forEach(function(value){
+        const li = document.createElement("li")
+        li.textContent = value
+        const i = document.createElement("i")
+        i.classList.add("fa-sharp", "fa-solid", "fa-xmark")
+        const span = document.createElement("span")
+        span.classList.add("deleteEntry", "btn", "btn-outline-warning", "d-flex", "border-0", "float-right")
+        span.appendChild(i)
+        li.appendChild(span)
+        ulList.appendChild(li)
+    })
 }

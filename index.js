07a712c25b81e7edb53ae609c3ee6c4b29908adb
Furkan <muhammedcengiz1@gmail.com>
2022-09-14 15:22:47 +0300

    trim metoduyla girdilerin boşluk olması engellendi

---
+++ index.js
@@ -0,0 +1,13 @@
+let inputToDo = document.getElementById("task")
+
+let ulList = document.getElementById("list")
+
+let toDo = []
+
+function newElement() {
+
+    if (inputToDo.value.trim()) {
+        
+    }
+
+}
